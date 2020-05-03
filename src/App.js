dea94c2615ecc6eddbe68c9bff5554c5d82f5a4a
aidanRockie <21993381+rockie2695@users.noreply.github.com>
2020-05-03 16:30:21 +0800

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -20,14 +20,17 @@ const App = () => {
   }
 
   useEffect(() => {
-    test()
+    if (Object.keys(login).length !== 0) {
+      console.log('do test')
+      test()
+    }
     if (ws) {
       //連線成功在 console 中打印訊息
       console.log('success connect!')
       //設定監聽
       initWebSocket()
     }
-  }, [ws]);
+  }, [ws, login]);
 
   const initWebSocket = () => {
     // Server 通知完後再傳送 disConnection 通知關閉連線
