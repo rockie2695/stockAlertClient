e2ba35ab3e1ecc97bfba34f2ce8d9ec51bd7fd9e
aidanRockie <21993381+rockie2695@users.noreply.github.com>
2020-06-28 00:46:14 +0800

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -554,7 +554,7 @@ const App = () => {
                     edit === true
                       ?
                       <Fragment>
-                        <Button variant="contained" color="primary" onClick={fun_save} disabled={sendingForm}>Save</Button>
+                        <Button variant="contained" color="primary" onClick={fun_save} disabled={sendingForm}>Save <CircularProgress size={20} /></Button>
                         <Button variant="contained" color="primary" onClick={fun_edit} disabled={sendingForm}>Cancel</Button>
                       </Fragment>
                       :
