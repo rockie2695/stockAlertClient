7a4ad788eafb7edeb5e75ccdbf154e0b32b2547c
aidanRockie <21993381+rockie2695@users.noreply.github.com>
2020-06-29 15:26:28 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -576,10 +576,14 @@ const App = () => {
                     </Grid>
                     <Hidden only="xs">
                       <Grid item xs={false} sm={2} md={2} className={classes.margin1}>
+                      </Grid>
+                      <Grid item xs={false} sm={2} md={2} className={classes.margin1}>
+                        <Typography>Alert</Typography>
+                      </Grid>
+                      <Grid item xs={false} sm={2} md={2} className={classes.margin1}>
                         <Typography>now$ to alert$</Typography>
                       </Grid>
-                      <Grid item xs={false} sm={5} md={5} className={classes.margin1}>
-                        <Typography>Alert</Typography>
+                      <Grid item xs={false} sm={1} md={1} className={classes.margin1}>
                       </Grid>
                     </Hidden>
                   </Grid>
