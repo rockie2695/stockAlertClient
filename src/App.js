25de5313675c4857afcca032f937c374aa48a78e
aidanRockie <21993381+rockie2695@users.noreply.github.com>
2020-07-14 23:00:18 +0800

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -36,6 +36,10 @@ import MuiDialogActions from '@material-ui/core/DialogActions';
 import CloseIcon from '@material-ui/icons/Close';
 import './App.css';
 
+import CanvasJSReact from './canvasjs.react';
+var CanvasJS = CanvasJSReact.CanvasJS;
+var CanvasJSChart = CanvasJSReact.CanvasJSChart;
+
 var host = 'https://rockie-stockAlertServer.herokuapp.com'
 
 const App = () => {
@@ -582,6 +586,54 @@ const App = () => {
     return [todayHour, todayMinute, todayDay, today, todaySecond]
   }
 
+  const options = {
+    animationEnabled: true,
+    exportEnabled: true,
+    theme: "light2", // "light1", "dark1", "dark2"
+    title: {
+      text: "Bounce Rate by Week of Year"
+    },
+    axisY: {
+      title: "Bounce Rate",
+      includeZero: false,
+      suffix: "%"
+    },
+    axisX: {
+      title: "Week of Year",
+      prefix: "W",
+      interval: 2
+    },
+    data: [{
+      type: "line",
+      toolTipContent: "Week {x}: {y}%",
+      dataPoints: [
+        { x: 1, y: 64 },
+        { x: 2, y: 61 },
+        { x: 3, y: 64 },
+        { x: 4, y: 62 },
+        { x: 5, y: 64 },
+        { x: 6, y: 60 },
+        { x: 7, y: 58 },
+        { x: 8, y: 59 },
+        { x: 9, y: 53 },
+        { x: 10, y: 54 },
+        { x: 11, y: 61 },
+        { x: 12, y: 60 },
+        { x: 13, y: 55 },
+        { x: 14, y: 60 },
+        { x: 15, y: 56 },
+        { x: 16, y: 60 },
+        { x: 17, y: 59.5 },
+        { x: 18, y: 63 },
+        { x: 19, y: 58 },
+        { x: 20, y: 54 },
+        { x: 21, y: 59 },
+        { x: 22, y: 64 },
+        { x: 23, y: 59 }
+      ]
+    }]
+  }
+
   /*
   you can make style like this
   const boxStyle={
@@ -889,6 +941,8 @@ const App = () => {
           <Typography gutterBottom>
             Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis
             in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+            <CanvasJSChart options={options} />
+
           </Typography>
           <Typography gutterBottom>
             Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis
