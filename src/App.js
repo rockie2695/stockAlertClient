3af3ece65610dd7e9644a24c55d468c1e9b1bdfb
aidanRockie <21993381+rockie2695@users.noreply.github.com>
2020-07-16 23:05:09 +0800

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -949,8 +949,8 @@ const App = () => {
             <CanvasJSChart options={options} />
 
 
-            <LineChart width={600} height={300} data={data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
-              <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+            <LineChart width={600} height={300} data={data} margin={{ top: 5, right: 0, bottom: 5, left: 0 }}>
+              <Line type="monotone" dataKey="uv" stroke="#8884d8" activeDot={{ r: 8 }} />
               <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
               <XAxis dataKey="name" />
               <YAxis />
