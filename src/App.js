fe229b49dcaeb451c188fa2bcb4cee58da6b375c
aidanRockie <21993381+rockie2695@users.noreply.github.com>
2020-07-16 22:33:46 +0800

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -37,8 +37,13 @@ import CloseIcon from '@material-ui/icons/Close';
 import './App.css';
 
 import CanvasJSReact from './canvasjs.react';
+
+import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts';
+
 var CanvasJS = CanvasJSReact.CanvasJS;
 var CanvasJSChart = CanvasJSReact.CanvasJSChart;
+
+const data = [{ name: 'Page A', uv: 400, pv: 2400, amt: 2400 }, { name: 'Page B', uv: 300, pv: 2400, amt: 2400 }, { name: 'Page C', uv: 200, pv: 2400, amt: 2400 }];
 
 var host = 'https://rockie-stockAlertServer.herokuapp.com'
 
@@ -942,6 +947,15 @@ const App = () => {
             Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis
             in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
             <CanvasJSChart options={options} />
+
+
+            <LineChart width={600} height={300} data={data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
+              <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+              <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+              <XAxis dataKey="name" />
+              <YAxis />
+              <Tooltip />
+            </LineChart>
 
           </Typography>
           <Typography gutterBottom>
