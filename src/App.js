5581191123bf04fa1a86c37a4af6d75ff3494f25
aidanRockie <21993381+rockie2695@users.noreply.github.com>
2020-08-15 17:43:45 +0800

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -81,10 +81,11 @@ const App = () => {
     window.addEventListener('offline', handleConnectionChange)
 
     window.addEventListener('beforeinstallprompt', (e) => {
+      // Prevent Chrome 67 and earlier from automatically showing the prompt
+      e.preventDefault();
       // Stash the event so it can be triggered later.
       console.log('beforeinstallprompt', e)
       setDeferredPrompt(prevState => e)
-      showA2HS(e)
     })
   }, []);
 
