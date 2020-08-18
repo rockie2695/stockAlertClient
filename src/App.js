ffd99ea921299bd45fc5d3ec3670c2f951e73253
aidanRockie <21993381+rockie2695@users.noreply.github.com>
2020-08-18 16:24:00 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1053,7 +1053,7 @@ const App = () => {
                 <LineChart data={selectHistory} margin={{ top: 10, right: 25, bottom: 10, left: 0 }}>
                   <Line type="monotone" dataKey="price" stroke="#8884d8" activeDot={{ r: 8 }} dot={false} />
                   <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
-                  <XAxis dataKey="jsTime" tickFormatter={(unixTime) => moment(unixTime).format('HH:mm')} type='number' scale='time' domain={['dataMin', 'dataMax']}  interval={(selectHistory[0].jsTime.getTime()-selectHistory[selectHistory.length-1].jsTime.getTime())/5}/>
+                  <XAxis dataKey="jsTime" tickFormatter={(unixTime) => moment(unixTime).format('HH:mm')} type='number' scale='time' domain={['dataMin', 'dataMax']}  interval={(selectHistory[selectHistory.length-1].jsTime.getTime()-selectHistory[0].jsTime.getTime())/5}/>
                   <YAxis domain={['auto', 'auto']} />
                   <Tooltip labelFormatter={(unixTime) => moment(unixTime).format('HH:mm')} />
                 </LineChart>
