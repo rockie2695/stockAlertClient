47ac1e436be9c198e81e64bc34ede1a03a5dd457
aidanRockie <21993381+rockie2695@users.noreply.github.com>
2020-08-27 09:38:55 +0800

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -781,6 +781,13 @@ const App = () => {
               outputSize: 1,
             }
           );
+          let net2 = new brain.recurrent.RNNTimeStep(
+            {
+              inputSize: 1,
+              hiddenLayers: [10],
+              outputSize: 1,
+            }
+          );
           //let net2 = new brain.recurrent.LSTMTimeStep();
           //let net3 = new brain.recurrent.GRUTimeStep();
           if (result.ok.length > 100) {
@@ -799,7 +806,9 @@ const App = () => {
             setTimeout(() => {
               net1.train([/*normalisedHP2*/[0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7]], { log: true });//default iterations: 20000
             }, 2000);
-
+            setTimeout(() => {
+              net2.train([normalisedHP2], { log: true });//default iterations: 20000
+            }, 2000);
             //net2.train(normalisedHP, { log: false });
             //net3.train(normalisedHP, { log: false });
 
