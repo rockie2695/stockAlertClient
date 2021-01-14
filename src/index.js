ead9926a39a932e2794be9520f609d271f13317c
dicky lee <dickylee@softmedia.hk>
2021-01-14 15:39:44 +0800

    remove react-snap which is not work in heroku

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM, { hydrate } from "react-dom";
+import ReactDOM /*, { hydrate } */ from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
@@ -8,7 +8,7 @@ import { BrowserRouter } from "react-router-dom";
 require("dotenv").config();
 
 const rootElement = document.getElementById("root");
-if (rootElement.hasChildNodes()) {
+/*if (rootElement.hasChildNodes()) {
   hydrate(
     <React.StrictMode>
       <BrowserRouter>
@@ -17,16 +17,16 @@ if (rootElement.hasChildNodes()) {
     </React.StrictMode>,
     rootElement
   );
-} else {
-  ReactDOM.render(
-    <React.StrictMode>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </React.StrictMode>,
-    rootElement
-  );
-}
+} else {*/
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+  rootElement
+);
+//}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
